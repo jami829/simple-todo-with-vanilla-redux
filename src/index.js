4492b629f657c74a99d20a3c0af4728ba712c912
이재훈 <jami8291215@gmail.com>
2021-01-05 23:12:42 +0900

    :art: action 함수 모듈화

--- src/index.js
+++ src/index.js
@@ -6,11 +6,26 @@ const ul = document.querySelector("ul");
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+// action을 함수화 시켜서 재사용성을 높이자.
+// dispatch 할 때 함수만 입력하면 된다. 
+const addToDo = text => {   // 파라미터인 text에 추후 새로 작성된 todo가 입력될것임. onSubmit 함수 잘 볼 것.
+  return {
+    type: ADD_TODO,
+    text
+  };
+};
+const deleteToDo = id => {
+  return {
+    type: DELETE_TODO,
+    id
+  };
+};
+
 const reducer = (state = [], action) => {
   // console.log(action)
   switch (action.type) {
     case ADD_TODO:
-      return [...state, { text: action.text, id: Date.now() }];  // 원형 배열이 절대 변형되면 안된다.(immutable), .text는 29번줄으로부터 왔음.
+      return [{ text: action.text, id: Date.now() }, ...state];  // 원형 배열이 절대 변형되면 안된다.(immutable), .text는 29번줄으로부터 왔음.
     case DELETE_TODO:
       return [];
     default:
